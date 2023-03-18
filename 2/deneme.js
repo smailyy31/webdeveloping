ebff460d894d687823b8166a49d5e9a4cca687f0
smailyy31 <ismailyesilyurt65@gmail.com>
2023-03-18 22:09:57 +0300

    new

---
+++ 2/deneme.js
@@ -0,0 +1,12 @@
+("Vize 1 notunuzu giriniz:")
+let vize2= prompt ("Vize 2 notunuzu giriniz:")
+let final= prompt ("final notunuzu giriniz:")
+
+
+
+if((((Number(vize1))*3/10 ) + ((Number(vize2))*3/10 ) +((Number(final))*4/10 ) ) > 60  ){
+   alert(" Sınavı geçtin yarram")
+}
+else{
+  alert( "kaldın bro") 
+}
